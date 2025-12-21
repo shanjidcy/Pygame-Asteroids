d20cfc593fcc47d1e947fd7d249591fc1c820a1e
shanjidcy <shanjidchowdhury@gmail.com>
2025-12-20 22:22:43 -0500

    Fixed main.py so everything is under main()

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
