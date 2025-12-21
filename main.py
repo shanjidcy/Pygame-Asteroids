e9089ea72ed75f6ae4990948ea055f983f60302e
shanjidcy <shanjidchowdhury@gmail.com>
2025-12-21 16:43:02 -0500

    White arrow to indicate player added

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import pygame
 from logger import log_state
 from constants import *
+from player import *
 
-pygame.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
-    
 
 def main():
     pygame.init()
@@ -17,14 +12,26 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player( SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2 )
+
+
     while True:
         log_state()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill("black")
-    pygame.display.flip()
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        print(dt)
+
 
 
 if __name__ == "__main__":
