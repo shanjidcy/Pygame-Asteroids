7738046f8a90a0c13ae409baaa611f3bda6ff1f6
shanjidcy <shanjidchowdhury@gmail.com>
2025-12-21 19:38:25 -0500

    added asteroid sprites to appear on screen

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from logger import log_state
 from constants import *
 from player import *
+from asteroidfield import *
 
 
 def main():
@@ -15,8 +16,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    player = Player( SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2 )
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
+    player = Player( SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2 )
 
     while True:
         log_state()
@@ -25,9 +34,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
